73f6b921766fa052370f92b18aebea2d641758fe
Project Fi <30879697+projectfi@users.noreply.github.com>
2017-08-09 19:48:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+window.location = "https://g.co/fi/r/5K3843";
+console.log("%cProject Fi - a wireless service from google_logo", "color:#4285F4; font-size:30px; font-family:'Product Sans', Arial, sans-serif;");
